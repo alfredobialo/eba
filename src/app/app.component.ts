334e73081342204d7b2d05554828bf2296573bab
Alfred Obialo <alfredcsdinc@gmail.com>
2024-03-09 13:13:09 +0100

    Setup chess board with Chess Pieces

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
-import {Component, signal} from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
 import {ChessBoardComponent} from "./features/components/chess-board/chess-board.component";
 import {ChessConfigService} from "./features/components/chess-board/ChessConfig";
-import {ChessBoardModel} from "./shared/models/chess-board-model";
+import {ChessBoardModel, ChessPieceBase, ChessPieceColor, ChessPieceType} from "./shared/models/chess-board-model";
 
 @Component({
   selector: 'app-root',
@@ -25,6 +25,13 @@ constructor(private config : ChessConfigService) {
 }
 ngOnInit(){
   ChessBoardModel.printChessBoard();
+
+  const chessPiecePawn = ChessPieceBase.createNewPiece(ChessPieceType.PAWN, ChessPieceColor.BLACK_PIECE);
+  const chessPieceRook = ChessPieceBase.createNewPiece(ChessPieceType.ROOK, ChessPieceColor.WHITE_PIECE);
+
+  console.log("Created a new Chess Piece => ", chessPiecePawn);
+  console.log("Created a new Chess Piece => ", chessPieceRook);
+
   this.theme = this.config.getCurrentBoardConfig();
   console.log("app root theme value: => ",this.theme);
 }
--- src/app/shared/models/chess-board-model.ts
+++ src/app/shared/models/chess-board-model.ts
@@ -1,151 +1,247 @@
-export class ChessBoardModel {
-  private static boardSetup = [
-    [1,2,3,4,5,6,7,8],
-    [1,2,3,4,5,6,7,8],
-    [1,2,3,4,5,6,7,8],
-    [1,2,3,4,5,6,7,8],
-    [1,2,3,4,5,6,7,8],
-    [1,2,3,4,5,6,7,8],
-    [1,2,3,4,5,6,7,8],
-    [1,2,3,4,5,6,7,8]
-  ];
-  static printChessBoard(){
-    console.log(this.boardSetup)
-  }
+export enum ChessPieceType {
+  PAWN = "Pawn", QUEEN = "Queen", KING = "King", KNIGHT = "Knight", BISHOP = "Bishop", ROOK = "Rook"
 }
 
-export interface IChessCell{
+export enum ChessPieceColor {
+  WHITE_PIECE = "WHITE",
+  BLACK_PIECE = "BLACK"
+}
+
+
+
+
+export interface IChessCell {
   isWhiteCell: boolean,
-  rowLetter? : string,
-  colNumber? : number,
-  showRowLetter : boolean,
-  showColNumber : boolean,
+  rowLetter?: string,
+  colNumber?: number,
+  showRowLetter: boolean,
+  showColNumber: boolean,
+
   getCellAddress(): string;
+
   piece?: IChessPiece
 }
-export interface IChessPiece{
-  point : number,
-  getPieceType():string;
-  pieceImageCss?:string;
-  isWhite : boolean;
-  isKnight():boolean;
-  isPawn():boolean;
-  isKing():boolean;
-  isQueen():boolean;
-  isRook():boolean;
-  isBishop():boolean;
+
+export interface IChessPiece {
+  point: number,
+
+  getPieceType(): string;
+
+  pieceImageCss?: string;
+  isWhite: boolean;
+
+  isKnight(): boolean;
+
+  isPawn(): boolean;
+
+  isKing(): boolean;
+
+  isQueen(): boolean;
+
+  isRook(): boolean;
+
+  isBishop(): boolean;
 }
 
-export abstract class ChessPieceBase  implements IChessPiece{
-    isKnight(): boolean {
-        return this.getPieceType() === ChessPieceType.KNIGHT;
-    }
-    isPawn(): boolean {
-      return this.getPieceType() === ChessPieceType.PAWN;
-    }
-    isKing(): boolean {
-      return this.getPieceType() === ChessPieceType.KING;
-    }
-    isQueen(): boolean {
-      return this.getPieceType() === ChessPieceType.QUEEN;
-    }
-    isRook(): boolean {
-      return this.getPieceType() === ChessPieceType.ROOK;
-    }
-    isBishop(): boolean {
-      return this.getPieceType() === ChessPieceType.BISHOP;
-    }
-    point: number = 1;
-    abstract getPieceType(): string
-    pieceImageCss: string ="";
-    isWhite: boolean = true;
-    protected setPieceImageCss(){
-      const pieceColor = this.isWhite ? "white" : "black";
+export abstract class ChessPieceBase implements IChessPiece {
+  isKnight(): boolean {
+    return this.getPieceType() === ChessPieceType.KNIGHT;
+  }
 
-      let pieceType = pieceColor+"-"+this.getPieceType().toLowerCase();
+  isPawn(): boolean {
+    return this.getPieceType() === ChessPieceType.PAWN;
+  }
 
-      this.pieceImageCss = pieceColor+"-piece " + pieceType; // sample css class= white-piece white-pawn
+  isKing(): boolean {
+    return this.getPieceType() === ChessPieceType.KING;
+  }
+
+  isQueen(): boolean {
+    return this.getPieceType() === ChessPieceType.QUEEN;
+  }
+
+  isRook(): boolean {
+    return this.getPieceType() === ChessPieceType.ROOK;
+  }
+
+  isBishop(): boolean {
+    return this.getPieceType() === ChessPieceType.BISHOP;
+  }
+
+  point: number = 1;
+
+  abstract getPieceType(): string
+
+  pieceImageCss: string = "";
+  isWhite: boolean = true;
+
+  protected setPieceImageCss() {
+    const pieceColor = this.isWhite ? "white" : "black";
+
+    let pieceType = pieceColor + "-" + this.getPieceType().toLowerCase();
+
+    this.pieceImageCss = pieceColor + "-piece " + pieceType; // sample css class= white-piece white-pawn
+  }
+
+  public static createNewPiece(pieceType: ChessPieceType = ChessPieceType.PAWN, pieceColor: ChessPieceColor = ChessPieceColor.WHITE_PIECE): IChessPiece {
+
+    const isWhitePiece = pieceColor == ChessPieceColor.WHITE_PIECE;
+    switch (pieceType) {
+      case ChessPieceType.PAWN:
+        return new PawnChessPiece(isWhitePiece);
+      case ChessPieceType.KNIGHT:
+        return new KnightChessPiece(isWhitePiece);
+      case ChessPieceType.BISHOP:
+        return new BishopChessPiece(isWhitePiece);
+      case ChessPieceType.ROOK:
+        return new RookChessPiece(isWhitePiece);
+      case ChessPieceType.QUEEN:
+        return new QueenChessPiece(isWhitePiece);
+      case ChessPieceType.KING:
+        return new KingChessPiece(isWhitePiece);
+
+
     }
+
+  }
+
 
 }
-export class PawnChessPiece extends ChessPieceBase{
-  constructor(isWhitePiece:boolean = true) {
+
+export class PawnChessPiece extends ChessPieceBase {
+  constructor(isWhitePiece: boolean = true) {
     super();
     this.isWhite = isWhitePiece;
     this.point = 1;
     this.setPieceImageCss();
   }
-    override getPieceType(): string {
-        return ChessPieceType.PAWN;
-    }
+
+  override getPieceType(): string {
+    return ChessPieceType.PAWN;
+  }
 
 }
-export class KnightChessPiece extends ChessPieceBase{
-  constructor(isWhitePiece:boolean = true) {
+
+export class KnightChessPiece extends ChessPieceBase {
+  constructor(isWhitePiece: boolean = true) {
     super();
     this.isWhite = isWhitePiece;
     this.point = 3;
     this.setPieceImageCss();
   }
+
   override getPieceType(): string {
     return ChessPieceType.KNIGHT;
   }
 
 }
-export class BishopChessPiece extends ChessPieceBase{
-  constructor(isWhitePiece:boolean = true) {
+
+export class BishopChessPiece extends ChessPieceBase {
+  constructor(isWhitePiece: boolean = true) {
     super();
     this.isWhite = isWhitePiece;
     this.point = 3;
     this.setPieceImageCss();
   }
+
   override getPieceType(): string {
     return ChessPieceType.BISHOP;
   }
 
 }
 
-export class QueenChessPiece extends ChessPieceBase{
-  constructor(isWhitePiece:boolean = true) {
+export class QueenChessPiece extends ChessPieceBase {
+  constructor(isWhitePiece: boolean = true) {
     super();
     this.isWhite = isWhitePiece;
     this.point = 9;
     this.setPieceImageCss();
   }
+
   override getPieceType(): string {
     return ChessPieceType.QUEEN;
   }
 
 }
-export class KingChessPiece extends ChessPieceBase{
-  constructor(isWhitePiece:boolean = true) {
+
+export class KingChessPiece extends ChessPieceBase {
+  constructor(isWhitePiece: boolean = true) {
     super();
     this.isWhite = isWhitePiece;
     this.point = 0;
     this.setPieceImageCss();
   }
+
   override getPieceType(): string {
     return ChessPieceType.KING;
   }
 
 }
-export class RookChessPiece extends ChessPieceBase{
-  constructor(isWhitePiece:boolean = true) {
+
+export class RookChessPiece extends ChessPieceBase {
+  constructor(isWhitePiece: boolean = true) {
     super();
     this.isWhite = isWhitePiece;
     this.point = 5;
     this.setPieceImageCss();
   }
+
   override getPieceType(): string {
     return ChessPieceType.ROOK;
   }
 
 }
-export  enum ChessPieceType{
-  PAWN = "Pawn",QUEEN ="Queen", KING ="King", KNIGHT ="Knight", BISHOP="Bishop", ROOK="Rook"
-}
 
-export enum PieceType {
-  WHITE_PIECE= "WHITE",
-  BLACK_PIECE = "BLACK"
+export class ChessBoardModel {
+  private static boardSetup = [
+    //Black Sector With main Officials
+    [ChessPieceBase.createNewPiece(ChessPieceType.ROOK,ChessPieceColor.BLACK_PIECE),
+      ChessPieceBase.createNewPiece(ChessPieceType.KNIGHT,ChessPieceColor.BLACK_PIECE),
+      ChessPieceBase.createNewPiece(ChessPieceType.BISHOP,ChessPieceColor.BLACK_PIECE),
+      ChessPieceBase.createNewPiece(ChessPieceType.QUEEN,ChessPieceColor.BLACK_PIECE),
+      ChessPieceBase.createNewPiece(ChessPieceType.KING,ChessPieceColor.BLACK_PIECE),
+      ChessPieceBase.createNewPiece(ChessPieceType.BISHOP,ChessPieceColor.BLACK_PIECE),
+      ChessPieceBase.createNewPiece(ChessPieceType.KNIGHT,ChessPieceColor.BLACK_PIECE),
+      ChessPieceBase.createNewPiece(ChessPieceType.ROOK,ChessPieceColor.BLACK_PIECE)],
+
+    // Black Pawn Lineup
+    [ChessPieceBase.createNewPiece( ChessPieceType.PAWN,ChessPieceColor.BLACK_PIECE),
+      ChessPieceBase.createNewPiece(ChessPieceType.PAWN,ChessPieceColor.BLACK_PIECE),
+      ChessPieceBase.createNewPiece(ChessPieceType.PAWN,ChessPieceColor.BLACK_PIECE),
+      ChessPieceBase.createNewPiece(ChessPieceType.PAWN,ChessPieceColor.BLACK_PIECE),
+      ChessPieceBase.createNewPiece(ChessPieceType.PAWN,ChessPieceColor.BLACK_PIECE),
+      ChessPieceBase.createNewPiece(ChessPieceType.PAWN,ChessPieceColor.BLACK_PIECE),
+      ChessPieceBase.createNewPiece(ChessPieceType.PAWN,ChessPieceColor.BLACK_PIECE),
+      ChessPieceBase.createNewPiece(ChessPieceType.PAWN,ChessPieceColor.BLACK_PIECE)],
+
+
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+
+    // White Pawn Lineup
+    [ChessPieceBase.createNewPiece(),
+      ChessPieceBase.createNewPiece(),
+      ChessPieceBase.createNewPiece(),
+      ChessPieceBase.createNewPiece(),
+      ChessPieceBase.createNewPiece(),
+      ChessPieceBase.createNewPiece(),
+      ChessPieceBase.createNewPiece(),
+      ChessPieceBase.createNewPiece()],
+
+    //White Sector With main Officials
+    [ChessPieceBase.createNewPiece(ChessPieceType.ROOK),
+      ChessPieceBase.createNewPiece(ChessPieceType.KNIGHT),
+      ChessPieceBase.createNewPiece(ChessPieceType.BISHOP),
+      ChessPieceBase.createNewPiece(ChessPieceType.QUEEN),
+      ChessPieceBase.createNewPiece(ChessPieceType.KING),
+      ChessPieceBase.createNewPiece(ChessPieceType.BISHOP),
+      ChessPieceBase.createNewPiece(ChessPieceType.KNIGHT),
+      ChessPieceBase.createNewPiece(ChessPieceType.ROOK)]
+  ];
+
+  static printChessBoard() {
+    console.log(this.boardSetup)
+  }
 }
